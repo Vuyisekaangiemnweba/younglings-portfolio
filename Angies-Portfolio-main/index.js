cccd37cd0cf163c0d9d3d9b707dcfd79ff009e66
Vuyisekaangiemnweba <110176374+Vuyisekaangiemnweba@users.noreply.github.com>
2023-02-28 09:42:34 +0200

    Add files via upload

---
+++ Angies-Portfolio-main/index.js
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+
+<button onclick="myFunction()">Click Me!</button>
+
+<script>
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
+
+
+
+<script>
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
+</script>
+
+</script>
+
+</body>
+</html>
